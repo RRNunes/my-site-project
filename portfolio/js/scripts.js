2a35b0667812a26cfa98443e6ee56848062011ab
Katyanne R. R. Nunes <betanunes8@gmail.com>
2024-07-22 18:39:08 -0400

    correcoes gerais

--- portfolio/js/scripts.js
+++ portfolio/js/scripts.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const learnMoreBtn = document.getElementById("learnMoreBtn");
 
+    if (learnMoreBtn) {
+        learnMoreBtn.addEventListener("click", function() {
+            window.location.href = "about.html";
+        });
+    }
+
+    const nav = document.querySelector("nav");
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 50) {
+            nav.classList.add("scrolled");
+        } else {
+            nav.classList.remove("scrolled");
+        }
+    });
 });
