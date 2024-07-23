7f53aa28ca83eb4975026a5762149d4dc63d7428
Katyanne Roberta R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-07-23 00:30:02 +0000

    mudancas no js

--- portfolio/js/scripts.js
+++ portfolio/js/scripts.js
@@ -1,12 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const learnMoreBtn = document.getElementById("learnMoreBtn");
-
-    if (learnMoreBtn) {
-        learnMoreBtn.addEventListener("click", function() {
-            window.location.href = "about.html";
-        });
-    }
-
     const nav = document.querySelector("nav");
     window.addEventListener("scroll", function() {
         if (window.scrollY > 50) {
@@ -15,4 +7,18 @@ document.addEventListener("DOMContentLoaded", function() {
             nav.classList.remove("scrolled");
         }
     });
+
+    document.querySelectorAll('nav ul li a').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
 });
+
